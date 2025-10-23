5dcf269a7869238ccbbe8bfc977eaf7f41d28d02
DinkTutor <dinksiraelisa@gmail.com>
2025-10-23 15:33:17 +0300

    Free Resource Button is added to the video component

---
+++ src/Common/Landing_page/components/FreeResourcesButton.tsx
@@ -0,0 +1,7 @@
+export default function FreeResourcesButton() {
+  return (
+    <button className="flex-1 bg-[#174A5F] hover:bg-[#1a5a73] text-white font-poppins font-medium py-3 px-4 rounded-lg transition-colors">
+      Free Resources
+    </button>
+  )
+}
