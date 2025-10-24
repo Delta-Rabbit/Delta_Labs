c26b997ae0f9d11457fb80e7e44d552b5d993e4c
DinkTutor <dinksiraelisa@gmail.com>
2025-10-24 17:19:34 +0300

    Free resource button design updated

--- src/Common/Landing_page/components/FreeResourcesButton.tsx
+++ src/Common/Landing_page/components/FreeResourcesButton.tsx
@@ -20,7 +20,7 @@ export default function FreeResourcesButton({ className }: FreeResourcesButtonPr
         fontFamily: "Roboto, sans-serif",
         fontWeight: 600,
         fontSize: "16px",
-        padding: "10px 25px",
+        padding: "10px 24px",
         clipPath: "polygon(0 0, 100% 0, 81% 100%, 0% 100%)",
         border: "none",
       }}
