f4eb22d1d883b8597c791915168e2401bef9d662
dinksira <dinksiraelisa@gmail.com>
2025-11-05 18:14:01 +0300

    textfield and subscription card layout updated

--- src/Common/Landing_page/components/BotChat.tsx
+++ src/Common/Landing_page/components/BotChat.tsx
@@ -10,7 +10,7 @@ interface BotChatProps {
 }
 
 export default function BotChat({
-  textFieldOffsetY = "110px",
+  textFieldOffsetY = "180px",
   textFieldOffsetX = "0px",
   messageOffsetY = "-80px",
   messageOffsetX = "-185px",
