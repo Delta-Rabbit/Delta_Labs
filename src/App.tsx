5e24e4c8d66f538d271d419f4779ea673a7a6c57
dinksira <dinksiraelisa@gmail.com>
2025-11-24 21:08:38 +0300

    add RootDepartmentPage navigation and update App.tsx routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavigationProvider } from "./modules/School/components/NavigationContext";
+import { useNavigation } from "./modules/School/components/NavigationContext";
 import LandingPage from "./Common/Landing_page/pages/LandingPage";
+import MySchool from "./modules/School/pages/MySchooPage";
+import OffersPage from "./modules/School/pages/OffersPage";
+import BuyAndSellPage from "./modules/School/pages/BuyAndSellPage";
+import ArchivePage from "./modules/School/pages/ArchivePage";
+import AddSchoolPage from "./modules/School/pages/AddSchoolPage";
+import RootDepartmentPage from "./modules/School/pages/RootDepartmentPage";
 
-function App() {
-  return <LandingPage />;
+function PageContent() {
+  const { currentPage } = useNavigation();
+
+  switch (currentPage) {
+    case 'home':
+      return <MySchool />;
+    case 'offers':
+      return <OffersPage />;
+    case 'buy-sell':
+      return <BuyAndSellPage />;
+    case 'archive':
+      return <ArchivePage />;
+    case 'add-school':
+      return <AddSchoolPage />;
+    case 'root-department':
+      return <RootDepartmentPage />;
+    default:
+      return <MySchool />;
+  }
 }
 
-export default App;
+function AppContent() {
+  const [currentApp, setCurrentApp] = useState('landing');
+
+  useEffect(() => {
+    const handleNavigateToSchool = () => {
+      setCurrentApp('school');
+    };
+
+    const handleNavigateToLanding = () => {
+      setCurrentApp('landing');
+    };
+
+    window.addEventListener('navigateToSchool', handleNavigateToSchool);
+    window.addEventListener('navigateToLanding', handleNavigateToLanding);
+
+    return () => {
+      window.removeEventListener('navigateToSchool', handleNavigateToSchool);
+      window.removeEventListener('navigateToLanding', handleNavigateToLanding);
+    };
+  }, []);
+
+  return (
+    <div>
+      {currentApp === 'landing' && <LandingPage />}
+      {currentApp === 'school' && (
+        <NavigationProvider>
+          <PageContent />
+        </NavigationProvider>
+      )}
+    </div>
+  );
+}
+
+export default AppContent;
